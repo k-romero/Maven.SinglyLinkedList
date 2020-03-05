0a0b37fef4450a33cb99aff35cc794fa0ce7f9a0
k-romero <imkevinromero@gmail.com>
2020-03-04 19:09:33 -0500

    started on lab, completed tests for current methods :D

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,93 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<K> {
+    public static int size = 0;
+    public Node<K> first;
+    public Node<K> last;
+
+    public SinglyLinkedList() {
+    }
+
+    public boolean firstNodeDoesNotExist(){
+        return first == null;
+    }
+
+    public void add(K elementToAdd){
+        if(firstNodeDoesNotExist()){
+            Node<K> newNode = new Node(null, elementToAdd,null);
+            first = newNode;
+            last = newNode;
+            size++;
+        } else {
+            Node<K> lastNode = last;
+            Node<K> newNode = new Node<K>(lastNode, elementToAdd, null);
+            lastNode.next = newNode;
+            last = newNode;
+            size++;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public K getFirst() {
+        return first.theElement;
+    }
+
+    public K getLast() {
+        return last.theElement;
+    }
+
+    public Node<K> getFirstNode() {
+        return first;
+    }
+
+    public K getElementOfNodeByIndexOfNode(int index){
+        K result = null;
+        if(index == 0){
+            result = getFirst();
+        } else {
+            Node<K> currentNode = first.next;
+            for (int i = 0; i < size; i++) {
+                if(currentNode.index == index){
+                    result = currentNode.theElement;
+                } else {
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int getNodeIndex(Node<K> nodeToGetIndexFrom) {
+        return nodeToGetIndexFrom.index;
+    }
+
+
+
+    private static class Node<K> {
+        K theElement;
+        Node<K> next;
+        Node<K> prev;
+        int index;
+
+        Node(Node<K> prev, K element, Node<K> next) {
+            theElement = element;
+            this.next = next;
+            this.prev = prev;
+            if(size == 0){
+                index = 0;
+            }
+            else{
+                index = prev.index + 1;
+            }
+        }
+    }
 }
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,89 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void checkFirstNodeDoesNotExistTest(){
+       SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+       Assert.assertTrue(list.firstNodeDoesNotExist());
+    }
+
+    @Test
+    public void addToEmptyListTest(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        String elm1 = "First Element";
+        list.add(elm1);
+        int expected = 1;
+        int actual = list.getSize();
+        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(elm1, list.getFirst());
+        Assert.assertEquals(elm1, list.getLast());
+    }
+
+    @Test
+    public void addToListWithOneElementAlreadyPresentTest(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        String elm1 = "First Element";
+        list.add(elm1);
+        String elm2 = "Second Element";
+        list.add(elm2);
+        int expected = 2;
+        int actual = list.getSize();
+        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(elm1, list.getFirst());
+        Assert.assertEquals(elm2, list.getLast());
+    }
+
+    @Test
+    public void addToListWithManyElements(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        String elm1 = "First Element";
+        String elm2 = "Second Element";
+        String elm3 = "Third Element";
+        String elm4 = "Fourth Element";
+        list.add(elm1);
+        list.add(elm2);
+        list.add(elm3);
+        list.add(elm4);
+        int expected = 4;
+        int actual = list.getSize();
+        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(elm1, list.getFirst());
+        Assert.assertEquals(elm4, list.getLast());
+    }
+
+    @Test
+    public void checkIndexOfFirstElement(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        String elm1 = "First Element";
+        list.add(elm1);
+        int expected = 0;
+        int actual = list.getNodeIndex(list.getFirstNode());
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getElementByIndex(){
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        String elm1 = "First Element";
+        String elm2 = "Second Element";
+        String elm3 = "Third Element";
+        String elm4 = "Fourth Element";
+        list.add(elm1);
+        list.add(elm2);
+        list.add(elm3);
+        list.add(elm4);
+        String expected = "Fourth Element";
+        String actual = list.getElementOfNodeByIndexOfNode(3);
+        Assert.assertEquals(expected,actual);
+    }
+
+
+
 }
